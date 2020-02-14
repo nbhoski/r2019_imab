af030c5cf41229e2f43da7e41acdf1304f7c0601
nbhoski <nikhil.bhoski@mathworks.in>
2020-02-14 13:34:29 +0530

    dd

--- source/squareRoot.m
+++ source/squareRoot.m
@@ -1,15 +1,12 @@
 function rs = squareRoot(x)
 
-% rs = sqrt(x);
+rs = sqrt(x);
 
 if x < 0
 %     ME = MException('Negative value %s Not accepted',x);
 %     throw(ME)
 
     error('SQUAREROOT:INVALIDINPUT','Negative value %d Not accepted',x);
-else
-   rs = sqrt(x);
-   %rs = sqrt(x-1);
 end
 
 end
